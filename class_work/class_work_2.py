9c1148076ff4998c2b95e28fb49dfd628ffca5a0
Benedict Maigida <teyeiesla@gmail.com>
2025-07-18 13:24:36 +0100

    upload class_work

---
+++ class_work/class_work_2.py
@@ -0,0 +1,104 @@
+'''
+#Arithmetic Operators
+
+#Addition
+first_number = 3
+first_number += 3
+print(first_number)
+
+#subtraction
+first_number = 6
+first_number -= 3
+print(first_number)
+
+#mutiplication
+first_number = 3
+first_number *= 3
+print(first_number)
+
+#Division
+first_number = 9
+first_number /= 3
+print(first_number)
+
+#floor division
+first_number = 7
+first_number //= 3
+print(first_number)
+
+#Modulus
+first_number = 3
+first_number %= 3
+print(first_number)
+
+'''
+
+#ClassWork
+
+weekly_allowance = 2000
+
+books = 400
+weekly_allowance -= books
+
+money_found = 100
+weekly_allowance += money_found
+
+snacks = 250
+weekly_allowance -= snacks
+
+sibling = 0.25
+sibling *= weekly_allowance
+weekly_allowance -= sibling
+
+data = 3
+data = weekly_allowance / data
+weekly_allowance -= data
+
+savings_and_tithing = 2
+savings_and_tithing = weekly_allowance / 2
+weekly_allowance -= savings_and_tithing
+
+weekly_allowance %= 100
+print(weekly_allowance)
+
+
+'''
+weekly_allowance = 2000
+books = 400
+balance = weekly_allowance - books
+print(f"allowance: {weekly_allowance} - books: {books} = balance: {balance}\n")
+
+money_found = 100
+balance = balance + money_found
+print(f"balance: 1500 + money found: {money_found} = balance: {balance}\n")
+
+snacks = 250 
+balance = balance - snacks
+print(f"balance: 1600 - snacks: {snacks} = balance: {balance}\n")
+
+sibling = 0.25
+balance_1 = balance * sibling
+balance = balance - balance_1
+print(f"balance: 1350 * sibling: {sibling} = balance: {balance}\n")
+
+data = 3
+balance_2 = balance / data
+balance = balance - balance_2
+print(f"balance: 365 / data: {data}  = balance: {balance}\n")
+
+savings_and_tithing = 2
+balance_3 = balance / savings_and_tithing
+balance = balance - balance_3
+print(f"balance:120 / Savings and Tithing: {savings_and_tithing} = balance:  {balance}\n")
+
+fianlly = 100
+balance = balance % fianlly
+print(f" finally: {fianlly}  = balance: {balance}")
+'''
+
+
+
+
+
+
+
